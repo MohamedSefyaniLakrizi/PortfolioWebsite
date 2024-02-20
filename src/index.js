4ea9cede0d9b6917a145e26d7f72c8fcbe633590
MohamedSefyaniLakrizi <mohamedsefyani@gmail.com>
2024-02-20 21:00:05 +0100

    init firebase

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Stars from './components/Stars';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Stars from "./components/Stars";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAeG7BWiXXRh_qo8T6ligUCDIJQz4xibuM",
+  authDomain: "mohamedsefyaniportfolio.firebaseapp.com",
+  projectId: "mohamedsefyaniportfolio",
+  storageBucket: "mohamedsefyaniportfolio.appspot.com",
+  messagingSenderId: "459317818195",
+  appId: "1:459317818195:web:77e86b351b3545954867a6",
+  measurementId: "G-H4LWE0F32E",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Stars />
     <App />
-    
   </React.StrictMode>
 );
 
