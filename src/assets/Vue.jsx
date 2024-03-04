fe0cf2c3cb1b4b4a0009315a572f28057d75fa51
MohamedSefyaniLakrizi <mohamedsefyani@gmail.com>
2024-03-04 17:13:39 +0100

    added 3rd project

--- src/assets/Vue.jsx
+++ src/assets/Vue.jsx
@@ -1,42 +1,45 @@
 export default function Vue({ className }) {
-  <svg
-    viewBox="0 -17.5 256 256"
-    version="1.1"
-    xmlns="http://www.w3.org/2000/svg"
-    xmlns:xlink="http://www.w3.org/1999/xlink"
-    preserveAspectRatio="xMidYMid"
-    fill="#000000"
-    className={`transition-transform duration-500 transform group-hover:-translate-x-28 ${className}`}
-  >
-    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-    <g
-      id="SVGRepo_tracerCarrier"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    ></g>
-    <g id="SVGRepo_iconCarrier">
-      {" "}
-      <g>
+  return (
+    <svg
+      viewBox="0 -17.5 256 256"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid"
+      fill="#000000"
+      height="25"
+      width="25"
+      className={`transition-transform duration-500 transform group-hover:-translate-x-28 ${className}`}
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
         {" "}
-        <path
-          d="M204.8,0 L256,0 L128,220.8 L0,0 L50.56,0 L97.92,0 L128,51.2 L157.44,0 L204.8,0 Z"
-          fill="#41B883"
-        >
+        <g>
           {" "}
-        </path>{" "}
-        <path
-          d="M0,0 L128,220.8 L256,0 L204.8,0 L128,132.48 L50.56,0 L0,0 Z"
-          fill="#41B883"
-        >
-          {" "}
-        </path>{" "}
-        <path
-          d="M50.56,0 L128,133.12 L204.8,0 L157.44,0 L128,51.2 L97.92,0 L50.56,0 Z"
-          fill="#35495E"
-        >
-          {" "}
-        </path>{" "}
-      </g>{" "}
-    </g>
-  </svg>;
+          <path
+            d="M204.8,0 L256,0 L128,220.8 L0,0 L50.56,0 L97.92,0 L128,51.2 L157.44,0 L204.8,0 Z"
+            fill="#41B883"
+          >
+            {" "}
+          </path>{" "}
+          <path
+            d="M0,0 L128,220.8 L256,0 L204.8,0 L128,132.48 L50.56,0 L0,0 Z"
+            fill="#41B883"
+          >
+            {" "}
+          </path>{" "}
+          <path
+            d="M50.56,0 L128,133.12 L204.8,0 L157.44,0 L128,51.2 L97.92,0 L50.56,0 Z"
+            fill="#35495E"
+          >
+            {" "}
+          </path>{" "}
+        </g>{" "}
+      </g>
+    </svg>
+  );
 }
